6834092bd1a4b80536d77534acbd5f20890c1979
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-02-01 21:19:04 +0600

    data insert and show without refresh page

--- djangocrudajax/urls.py
+++ djangocrudajax/urls.py
@@ -21,4 +21,5 @@ from enroll import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('save/', views.save_data, name='save'),
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -1,9 +1,30 @@
+import imp
+from django.http import JsonResponse
 from django.shortcuts import render
 from . forms import StudentRegistration
 from . models import User
+#from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     form = StudentRegistration()
     stud = User.objects.all();
-
     return render(request, 'enroll/home.html', {'form': form, 'stud': stud})
+
+#@csrf_exempt
+def save_data(request):
+    if request.method == 'POST':
+        form = StudentRegistration(request.POST)
+        if form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            password = request.POST['password']
+
+            usr = User(name = name, email = email, password = password)
+            usr.save()
+            stud = User.objects.values()
+            student_data = list(stud)
+            #print(student_data)
+            return JsonResponse({'status': 'Save', 'student_data':student_data})
+        else:
+            return JsonResponse({'status': 0})
+
