50176d84bd25a6a8523b5fcc6ac1ba6959da06ca
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-02-02 00:19:55 +0600

    ajax update function added

--- djangocrudajax/urls.py
+++ djangocrudajax/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('save/', views.save_data, name='save'),
     path('delete/', views.delete_data, name='delete'),
+    path('edit/', views.edit_data, name='edit'),
 
 ]
     #path('delete/<int:id>/', views.delete_data, name='delete'),
--- enroll/views.py
+++ enroll/views.py
@@ -16,11 +16,16 @@ def save_data(request):
     if request.method == 'POST':
         form = StudentRegistration(request.POST)
         if form.is_valid():
+            sid = request.POST.get('stuid')
             name = request.POST['name']
             email = request.POST['email']
             password = request.POST['password']
 
-            usr = User(name = name, email = email, password = password)
+            if sid == '':
+                usr = User(name = name, email = email, password = password)
+            else:
+                 usr = User(id = sid, name = name, email = email, password = password)
+
             usr.save()
             stud = User.objects.values()
             student_data = list(stud)
@@ -44,4 +49,17 @@ def delete_data(request):
      else:    
 
         return JsonResponse({'status': 0})
+
+
+
+def edit_data(request):
+    if request.method == "POST":
+        id = request.POST.get('sid')
+        print(id)
+
+        student = User.objects.get(id = id)
+        student_data = {"id": student.id, "name": student.name, "email": student.email, "password":student.password}
+
+        return JsonResponse(student_data)
+
 
