2b361faa2d6f8729717b0c757cf2fc8b058f5e20
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-02-01 22:57:49 +0600

    single page delete added

--- djangocrudajax/urls.py
+++ djangocrudajax/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('save/', views.save_data, name='save'),
+    path('delete/', views.delete_data, name='delete'),
+
 ]
+    #path('delete/<int:id>/', views.delete_data, name='delete'),
--- enroll/views.py
+++ enroll/views.py
@@ -1,9 +1,10 @@
 import imp
-from django.http import JsonResponse
+from unittest import registerResult
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from . forms import StudentRegistration
 from . models import User
-#from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     form = StudentRegistration()
@@ -27,4 +28,20 @@ def save_data(request):
             return JsonResponse({'status': 'Save', 'student_data':student_data})
         else:
             return JsonResponse({'status': 0})
+
+#@csrf_exempt
+def delete_data(request):
+
+   # print(request.POST.get('sid'))
+    
+     if request.method == 'POST':
+         id = request.POST.get('sid')
+         print(id)
+         pi = User.objects.get(id=id)
+         pi.delete()
+
+         return JsonResponse({'status': 1})
+     else:    
+
+        return JsonResponse({'status': 0})
 
